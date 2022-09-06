6f06024315092bde94823ffca8ea86cc0f98c048
jchoque <jonatec_92@hotmail.com>
2022-09-05 21:31:08 -0300

    agregado de registro y validaciones de logica

--- js/main.js
+++ js/main.js
@@ -1,25 +1,101 @@
-let usuario1 = "jonatan"
-let clave1 = "alumno"
-let usuario2 = "ian"
-let clave2 = "tutor"
-let valido
+let usuario1 = "ian"
+let clave1 = "tutor"
 let usuario
+let clave
+let msg
+let valido
 
+function menu (cant,o1,o2,o3,o4,o5) {
 
-do {
-    usuario = prompt ("Ingrese un Usuario valido")
-    let clave = prompt ("Ingrese su Clave")
-
-    if (usuario == usuario1 && clave == clave1 || usuario == usuario2 && clave == clave2) {
-        valido = true
+    switch (cant) {
+        
+        case 2:
+            msg = `Elija una opcion
+            1. ${o1}
+            2. ${o2}`
+            break;
+        case 3:
+            msg = `Elija una opcion
+            1. ${o1}
+            2. ${o2}
+            3. ${o3}`
+            break;
+        case 4:
+            msg = `Elija una opcion
+            1. ${o1}
+            2. ${o2}
+            3. ${o3}
+            4. ${o4}`
+            break;
+        case 5:
+            msg = `Elija una opcion
+            1. ${o1}
+            2. ${o2}
+            3. ${o3}
+            4. ${o4}
+            5. ${o5}`
+            break
+        default:
+            console.log ("ingrese la cantidad de opciones entre 2 y 5")
+            break;
     }
-    else {
-        valido = false
-        alert ("Usuario o Clave invalido!")
-    }
+    
+    return (msg)
 
+}
 
-} while (!valido);
+function SolUsuario() {
+    usuario = prompt ("Ingrese usuario")
+    clave = prompt ("Ingrese Clave")
+    valido = true
+    return (usuario,clave)
+}
 
+function valida() {
+    
+    do {
+        usuario = prompt ("Ingrese su usuario o esc para salir")
+        if (usuario == "esc"){
+            valido = false
+            break;
+        }else;
+        clave = prompt ("Ingrese su clave")
+        if (usuario == usuario1 && clave == clave1) {
+            valido = true
+        }
+        else {
+            valido = false
+            alert ("Usuario o Clave invalido!")
+        }
 
-alert (`Bienvenido ${usuario}`)
+    } while (!valido);
+}
+
+menu (2,"Ingresar","Registro",0,0,0)
+let eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
+Para operar en nuestro sitio debe contar con un usuario.
+${msg}`))
+
+while (eleccion !== 1 && eleccion !== 2) {
+     alert ("Ingrese una opcion valida")
+     eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
+     Para operar en nuestro sitio debe contar con un usuario.
+     ${msg}`))
+}
+
+if (eleccion == 1) {
+    valida ()
+}
+else if (eleccion == 2) {
+    SolUsuario()
+    usuario1 = usuario
+    clave1 = clave
+}
+else;
+
+if (valido) {
+    alert (`Bienvenido ${usuario}!`)
+}else {
+    alert ('Hasta pronto!')
+}
+
