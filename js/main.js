55cd7a310e3a13b7dca90bdf9ea734608f6482a1
jchoque <jonatec_92@hotmail.com>
2022-09-22 12:23:04 -0300

    subida del proyecto gral, agregado de eventos para el inicio de sesion

--- js/main.js
+++ js/main.js
@@ -3,49 +3,94 @@ const usuario = {usuario:"",clave:""}
 let msg
 let valido
 let bandera
+let logdo = 0
 
-function users (usuario,clave) {
-    this.usuario = usuario;
-    this.clave = clave
-}
+logueado()
 
-function menu (cant,o1,o2,o3,o4,o5) {
+/* Modal */
+const abrir = document.querySelector(".login")
+const cerrar = document.querySelector(".cruze")
+const modal = document.querySelector(".modal-login")
+const modalC = document.querySelector(".modal-container")
+const msj = document.querySelector(".msj")
 
-    switch (cant) {
-        
-        case 2:
-            msg = `Elija una opcion
-            1. ${o1}
-            2. ${o2}`
-            break;
-        case 3:
-            msg = `Elija una opcion
-            1. ${o1}
-            2. ${o2}
-            3. ${o3}`
-            break;
-        case 4:
-            msg = `Elija una opcion
-            1. ${o1}
-            2. ${o2}
-            3. ${o3}
-            4. ${o4}`
-            break;
-        case 5:
-            msg = `Elija una opcion
-            1. ${o1}
-            2. ${o2}
-            3. ${o3}
-            4. ${o4}
-            5. ${o5}`
-            break
-        default:
-            console.log ("ingrese la cantidad de opciones entre 2 y 5")
-            break;
+abrir.addEventListener("click", (e) => {
+    e.preventDefault()
+    modalC.style.visibility = "visible";
+    modal.classList.toggle("modal-close");
+})
+
+cerrar.addEventListener("click",() => {
+    modal.classList.toggle("modal-close");
+    setTimeout(() => {
+        modalC.style.visibility = "hidden";
+        if (bandera == 1) {
+            msj.classList.toggle("alert-danger");
+            msj.innerText = "Si Usted ya se encuentra registrado, por favor complete su Usuario y Clave.";
+            bandera = 0
+        }
+    },500)
+})
+
+window.addEventListener("click", (e) => {
+    if (e.target == modalC) {
+        modal.classList.toggle("modal-close");
+        setTimeout(() => {
+            modalC.style.visibility = "hidden";
+            if (bandera == 1) {
+                msj.classList.toggle("alert-danger");
+                msj.innerText = "Si Usted ya se encuentra registrado, por favor complete su Usuario y Clave.";
+                bandera = 0
+            }
+        },500)
     }
-    
-    return (msg)
+})
 
+const formLogin = document.querySelector("#formLogin")
+
+formLogin.addEventListener("submit", (e) => {
+    e.preventDefault()
+    usuario.usuario = formLogin.usuario.value 
+    usuario.clave = formLogin.clave.value
+    valida(usuario.usuario,usuario.clave)
+    if (valido==1) {
+        if (bandera == 1) {msj.classList.toggle("alert-danger");}
+        msj.classList.toggle("alert-success");
+        msj.innerText = "Login Exitoso!";
+        setTimeout(() => {
+            modal.classList.toggle("modal-close");
+        },800)
+        setTimeout(() => {
+            modalC.style.visibility = "hidden";
+        },1300)
+        logdo = 1;
+        logueado()
+    }else{
+        if (bandera !== 1){msj.classList.toggle("alert-danger");}
+        msj.innerText = "Usuario/Clave incorrecto";
+        formLogin.usuario.value = ""
+        formLogin.clave.value = ""
+        bandera=1
+    }
+})
+
+const logout = document.querySelector(".logout")
+
+logout.addEventListener("click",(e) => {
+    logdo = 0;
+    logueado()
+})
+
+function logueado(){
+    const logIN = document.querySelector(".in")
+    const logOUT = document.querySelector(".out")
+    if (logdo == 1){
+        logIN.classList.remove("on")
+        logOUT.classList.add("on")
+    }else{
+        logIN.classList.add("on")
+        logOUT.classList.remove("on")
+    }
 }
 
 function SolUsuario() {
@@ -70,58 +115,38 @@ function SolUsuario() {
     
 }
 
-function valida() {
-    
-    do {
-        bandera = 0
-        usuario.usuario = prompt ("Ingrese su usuario o esc para salir")
-        if (usuario.usuario == "esc"){
-            valido = false
-            break;
-        }else;
-        usuario.clave = prompt ("Ingrese su clave")
+function valida(user,pass) {
+    valido = 0
+    for (const registrado of registrados) {
+        if (user == registrado.usuario && pass == registrado.clave) {valido = 1}
+    return (valido);
+}}
 
-        for (const registrado of registrados) {
-            if (registrado.usuario == usuario.usuario && registrado.clave == usuario.clave) {bandera = 1}
-        }
+// menu (2,"Ingresar","Registro",0,0,0)
+// let eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
+// Para operar en nuestro sitio debe contar con un usuario.
+// ${msg}`))
 
-        if (bandera == 1) {
-            valido = true
-        }
-        else {
-            valido = false
-            alert ("Usuario o Clave invalido!")
-        }
+// while (eleccion !== 1 && eleccion !== 2) {
+//      alert ("Ingrese una opcion valida")
+//      eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
+//      Para operar en nuestro sitio debe contar con un usuario.
+//      ${msg}`))
+// }
 
-    } while (!valido);
-}
+// if (eleccion == 1) {
+//     valida ()
+// }
+// else if (eleccion == 2) {
+//     SolUsuario()
+// }
+// else;
 
-menu (2,"Ingresar","Registro",0,0,0)
-let eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
-Para operar en nuestro sitio debe contar con un usuario.
-${msg}`))
+// if (valido) {
+//     alert (`Proceso exitoso!`)
+//     const exito = document.querySelector(".main")
+//     exito.innerHTML = `<h1>Bienvenido ${usuario.usuario} </h1>`
 
-while (eleccion !== 1 && eleccion !== 2) {
-     alert ("Ingrese una opcion valida")
-     eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
-     Para operar en nuestro sitio debe contar con un usuario.
-     ${msg}`))
-}
-
-if (eleccion == 1) {
-    valida ()
-}
-else if (eleccion == 2) {
-    SolUsuario()
-}
-else;
-
-if (valido) {
-    alert (`Proceso exitoso!`)
-    const exito = document.querySelector(".main")
-    exito.innerHTML = `<h1>Bienvenido ${usuario.usuario} </h1>`
-
-}else {
-    alert ('Hasta pronto!')
-}
-
+// }else {
+//     alert ('Hasta pronto!')
+// }
