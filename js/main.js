1c66cf147a7f9c17735f2415894ae14d7e1607b9
jchoque <jonatec_92@hotmail.com>
2022-09-27 00:56:21 -0300

    se cargaron las paginas faltantes, se termino el proceso de registro y logueo

--- js/main.js
+++ js/main.js
@@ -2,16 +2,10 @@ const usuario = {usuario:"",clave:""}
 let msg
 let valido
 let bandera
-// localStorage.removeItem('usuarioActivo')
+
 logueado()
 
-/* Modal */
-const abrir = document.querySelector(".login")
-const cerrar = document.querySelector(".cruze")
-const modal = document.querySelector(".modal-login")
-const modalC = document.querySelector(".modal-container")
-const msj = document.querySelector(".msj")
-
+// Mensaje sobre el icono del carrito
 const registrese = document.querySelector(".cart")
 registrese.addEventListener("mouseover",() => {
     const on = document.querySelector(".registrese")
@@ -21,6 +15,13 @@ registrese.addEventListener("mouseout",() => {
     const on = document.querySelector(".registrese")
     on.classList.add("on")
 })
+
+/* Modal */
+const abrir = document.querySelector(".login")
+const cerrar = document.querySelector(".cruze")
+const modal = document.querySelector(".modal-login")
+const modalC = document.querySelector(".modal-container")
+const msj = document.querySelector(".msj")
 
 
 abrir.addEventListener("click", (e) => {
@@ -61,7 +62,7 @@ formLogin.addEventListener("submit", (e) => {
     e.preventDefault()
     usuario.usuario = formLogin.usuario.value 
     usuario.clave = formLogin.clave.value
-    valida(usuario.usuario,usuario.clave)
+    validaUsuarioClave(usuario.usuario,usuario.clave,2)
     if (valido==1) {
         if (bandera == 1) {msj.classList.toggle("alert-danger");}
         msj.classList.toggle("alert-success");
@@ -91,14 +92,28 @@ logout.addEventListener("click",(e) => {
 })
 
 function logueado(){
+    
     let userAct = localStorage.getItem('userAct')
     const logIN = document.querySelector(".in")
     const logOUT = document.querySelector(".out")
+    let nombre
+    let apellido
     if (userAct !== null){
         logIN.classList.remove("on")
         logOUT.classList.add("on")
+        const registrados = JSON.parse(localStorage.getItem('registrados')) || []
+        for (const registrado of registrados) {
+            if (registrado.email == userAct) { 
+                nombre = registrado.nombre;
+                apellido = registrado.apellido;
+                break;
+            }else {
+                nombre = userAct
+                apellido = ""
+            }
+        }
         const nombreUsuario = document.querySelector(".nombreUsuario")
-        nombreUsuario.innerText = userAct
+        nombreUsuario.innerText = `${nombre} ${apellido}`
     }else{
         logIN.classList.add("on")
         logOUT.classList.remove("on")
@@ -129,52 +144,78 @@ function SolUsuario() {
     
 }
 
-function valida(user,pass) {
+function validaUsuarioClave(user,pass,num) {
     const registrados = obtenerInfoUsuarios()
-    console.log(registrados)
     valido = 0
-    for (const registrado of registrados) {
-        if (user == registrado.usuario && pass == registrado.clave) {valido = 1}   
+    if (num == 1) {
+        for (const registrado of registrados) {
+            if (user == registrado.email) {valido = 1}   
+        }
+        return (valido);
+    }else if (num == 2){
+        for (const registrado of registrados) {
+            if (user == registrado.email && pass == registrado.clave) {valido = 1}   
+        }
+        return (valido);
     }
-    return (valido);
+}
+
+class cliente {
+    constructor(nombre, apellido, email, telefono, provincia, pais, clave) {
+        this.nombre = nombre
+        this.apellido = apellido
+        this.email = email
+        this.telefono = telefono
+        this.provincia = provincia
+        this.pais = pais
+        this.clave = clave
+    }
 }
 
 function obtenerInfoUsuarios(){
     let registrados = JSON.parse(localStorage.getItem('registrados'))
     if (registrados == null) {
+
         registrados = [
-            {usuario:"admin", clave: "1234"},
-            {usuario:"jonatan", clave: "2461"}
-        ]  
+            new cliente ("admin","admin","admin@admin","1111111111","buenos aires","argentina","1234")
+        ]
+        let registradosStrify = JSON.stringify(registrados)
+        localStorage.setItem('registrados',registradosStrify)
     }
     return registrados;
 }
 
-// menu (2,"Ingresar","Registro",0,0,0)
-// let eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
-// Para operar en nuestro sitio debe contar con un usuario.
-// ${msg}`))
-
-// while (eleccion !== 1 && eleccion !== 2) {
-//      alert ("Ingrese una opcion valida")
-//      eleccion = parseInt (prompt (`Bienvenido a JECMUSIC! 
-//      Para operar en nuestro sitio debe contar con un usuario.
-//      ${msg}`))
-// }
-
-// if (eleccion == 1) {
-//     valida ()
-// }
-// else if (eleccion == 2) {
-//     SolUsuario()
-// }
-// else;
-
-// if (valido) {
-//     alert (`Proceso exitoso!`)
-//     const exito = document.querySelector(".main")
-//     exito.innerHTML = `<h1>Bienvenido ${usuario.usuario} </h1>`
-
-// }else {
-//     alert ('Hasta pronto!')
-// }
+const registro = document.querySelector("#registro")
+registro.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let mismaClave
+    const msjReg = document.querySelector(".msjReg")
+    console.log(registro.telefono.value)
+    validaUsuarioClave(registro.email.value,0,1)
+    if (registro.clave.value == registro.clave2.value) {mismaClave = 1}else{mismaClave=0}
+    switch (true) {
+        case valido == 1 && mismaClave == 1:
+            msjReg.innerText = "El mail ya se encuentra registrado"
+            break;
+        case valido == 0 && mismaClave == 0:
+            msjReg.innerText = "Las claves no coinciden, porfavor ingrese la misma clave"
+            break;
+        case valido == 1 && mismaClave == 0:
+            msjReg.innerText = "El mail ya se encuentra registrado, ademas las claves no coinciden"
+            break;
+        case valido == 0 && mismaClave == 1:
+            let registrados = JSON.parse(localStorage.getItem('registrados')) || []
+            console.log(registrados)
+            registrados.push (new cliente (registro.nombre.value,registro.apellido.value,registro.email.value,registro.telefono.value,registro.provincia.value,registro.pais.value,registro.clave.value))
+            console.log(registrados)
+            let registradosStrify = JSON.stringify(registrados)
+            localStorage.setItem('registrados',registradosStrify)
+            localStorage.setItem('userAct',registro.email.value)
+            window.location.href="./index.html"
+            logueado()
+            break;   
+        default:
+            console.log("No se pudo chequear el usuario o la clave")
+            break;
+    }
+})
