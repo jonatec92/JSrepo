6f39853b466fb49f90a53eb1326ed0d5e022e3b3
jonatec92 <97567889+jonatec92@users.noreply.github.com>
2022-10-13 00:13:14 -0300

    Correccion del path del archivo json

--- js/productos.js
+++ js/productos.js
@@ -1,6 +1,6 @@
 const productos = document.querySelector(".productos")
 const cargarArticu = async () => {
-    const resp = await fetch('../data/articulos.json')
+    const resp = await fetch('./data/articulos.json')
     const articulos = await resp.json()
     armaListaProductos(articulos,productos)
 }
